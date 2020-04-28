fea3c0763ba6538287cbc2cc1f409760dde8eff0
Diveev Denis <ddiveev@icloud.com>
2020-04-29 00:28:50 +0300

    cosmetic

--- dolores/types/__init__.py
+++ dolores/types/__init__.py
@@ -1,2 +1,3 @@
 from dolores.platforms.vk.mixins.message import VkMessagesMixin
 from dolores.platforms.vk.types.message import VkMessageType
+from dolores.platforms.telegram.types.types import TgMessageType
--- views/view.py
+++ views/view.py
@@ -1,24 +1,18 @@
-from typing import Union
-
 from dolores.decorators import ViewHandler, MessageHandler
-from dolores.mixins.messages import MessagesMixin
-from dolores.platforms.telegram.types.types import TgMessageType
+from dolores.mixins import MessagesMixin
 from dolores.types import VkMessageType
 from dolores.views import View
 from tools.state import State
-
-
-Message = Union[VkMessageType, TgMessageType]
 
 
 @ViewHandler(state=State.START)
 class StartView(View, MessagesMixin):
 
     @MessageHandler(regex="[П,п]ривет")
-    async def hi(self, event: Message) -> None:
+    async def hi(self, event: VkMessageType) -> None:
         await self.send_message(self.user, "Привет")
 
     @MessageHandler
-    async def other(self, event: Message) -> None:
+    async def other(self, event: VkMessageType) -> None:
         await self.send_message(self.user, event.text)
 
